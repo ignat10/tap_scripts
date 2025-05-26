99537c6ecb34ec85e5b0b96c727e9a352142b2b2
Ignat <ignat.shikh@gmail.com>
2025-05-26 23:28:06 +0200

    I add something

---
+++ tap_WAO_farms.py
@@ -0,0 +1,41 @@
+import time
+import os
+
+
+ADB = "C: \\"  # дописать расположение ADB lenovo
+
+
+# add points to click
+point_take = (520, 1690)
+point_close = (500, 1925)
+point_help = (1000, 2100)
+point_lord = (1000, 1830)
+point_harvest = (200, 1560)
+point_use = (530, 2330)
+point_map = (100, 2300)
+point_search = (1000, 1981)
+point_iron = (600, 2075)
+point_stone = (425, 2075)
+point_wood = (256, 2075)
+point_food = (100, 2075)
+point_plus = (675, 2310)
+point_minus = (115, 2310)
+point_go_mine = (900, 2325)
+point_gather = (775, 1158)
+point_go = (900, 2315)
+point_favorites = (70, 1823)
+point_elite = (700, 365)
+point_elite1 = (380, 780)
+point_elite2 = (380, 1010)
+point_elite3 = (380, 1230)
+point_elite4 = (380, 1460)
+point_elite5 = (380, 1690)
+point_gather_elite = (770, 940)
+point_avatar = (90, 228)
+point_account = (180, 1100)
+point_switch = (550, 1528)
+point_login = (560, 1360)
+point_google = (145, 790)  #google 2,3,4,5 y = 930, 1130, 1330, 1530
+point_castle1 = (500, 1000)
+point_castle2 = (500, 1100)
+point_confirm = (313, 1372)
