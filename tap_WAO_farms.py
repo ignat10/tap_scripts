ff83a38f2b69578e6d41e38315245074ca2fd67a
Ignat <ignat.shikh@gmail.com>
2025-06-07 17:49:56 +0200

    full version for 8 farms with a lot of bugs

--- tap_WAO_farms.py
+++ tap_WAO_farms.py
@@ -28,26 +28,30 @@ point_gather = (775, 1158)
 point_go = (900, 2315)
 point_back = (790, 2121)
 point_favorites = (70, 1823)
+point_favourites_back = (70, 1823)
 point_elite = (700, 365)
-point_elite_mine = (380, 786) # point_elite_mine2, 3, 4, 5 = (380 1015, 1230, 1460, 1690) ++228
+point_elite_mine = (380, 785) # point_elite_mine2, 3, 4, 5 = (380 1015, 1230, 1460, 1690) ++228
 point_elite2 = (380, 1015)
 point_elite3 = (380, 1242)
 point_elite4 = (380, 1470)
 point_elite5 = (380, 1699)
 point_gather_elite = (770, 940)
+point_vip = (900, 1455)
 point_avatar = (90, 228)
 point_account = (180, 1100)
 point_switch = (550, 1528)
 point_login = (560, 1360)
-point_google = (145, 790)  #google 2,3,4,5 y = 930, 1130, 1330, 1530
-point_castle1 = (500, 1000)
-point_castle2 = (500, 1100)
+point_google = (145, 790)  # google 2,3,4,5 y = 930, 1130, 1330, 1530
+point_castle1 = (400, 1000)
+point_castle2 = (400, 1100)
 point_confirm = (313, 1372)
 
 # 6 lv minus that number
 lv = 0 # iron first than - that
 mine_type = 0
 color = 0
+witch_mine = 0
+acc = 1
 
 #functions
 def wait():
@@ -77,15 +81,18 @@ def find_another():
     time.sleep(1)
 
 def gather_mine():
-    global mine_type, lv
     os.system(f"{ADB} shell input tap {point_gather[0]} {point_gather[1]}")
-    time.sleep(0.5)
-    os.system(f"{ADB} shell input tap {point_go[0]} {point_go[1]}")
-    time.sleep(0.5)
-    os.system(f"{ADB} shell input tap {point_back[0]} {point_back[1]}")
+    if witch_mine > 3:
+        check_color(point_vip)
+        if not color == (0, 132, 162, 255):
+            os.system(f"{ADB} shell input tap {point_go[0]} {point_go[1]}")
+            os.system(f"{ADB} shell input tap {point_back[0]} {point_back[1]}")
+        else:
+            wait()
+            wait()
 
 def get_mine(): # to go to basic mine from the map
-    global mine_type, color, lv
+    global mine_type, color, lv, witch_mine, stop
     os.system(f"{ADB} shell input tap {point_search[0]} {point_search[1]}")  # At the map
     find_another()
     while True:
@@ -110,7 +117,7 @@ def get_mine(): # to go to basic mine from the map
                 break
             else:
                 wait()
-                os.system(f"adb shell input tap {point_mine[0]} {point_mine[1]}")
+                os.system(f"{ADB} shell input tap {point_mine[0]} {point_mine[1]}")
                 check_color(point_gather)    # check color of point_gather
                 if all(abs(a - t) < 5 for a, t in zip(color, (46, 37, 43, 255))):
                     gather_mine()
@@ -122,38 +129,74 @@ def get_mine(): # to go to basic mine from the map
                         mine_type = 0
                         lv += 1
                     find_another()
+    witch_mine += 1
+
+def second_farm():
+    global point_google, acc
+    acc = 1
+    point_google = (145, 790)  # google 2,3,4,5 y = 930, 1130, 1330, 1530
+    os.system(f"{ADB} shell input tap {point_avatar[0]} {point_avatar[1]}")
+    os.system(f"{ADB} shell input tap {point_account[0]} {point_account[1]}")
+    while True:
+        os.system(f"{ADB} shell input tap {point_switch[0]} {point_switch[1]}")
+        os.system(f"{ADB} shell input tap {point_login[0]} {point_login[1]}")
+        time.sleep(1)
+        os.system(f"{ADB} shell input tap {point_google[0]} {point_google[1]}")
+        if acc == 1:
+            os.system(f"{ADB} shell input tap {point_castle1[0]} {point_castle1[1]}")
+            acc = 2
+            break
+        else:        # if 1 acc is used
+            check_color(point_castle2)
+            if color == (42, 74, 102, 255):# if 2 acc exists
+               os.system(f"{ADB} shell input tap {point_castle2[0]} {point_castle2[1]}")# go inside
+               break
+            else:   # if 2 acc is not exists
+                wait()# close menu
+            acc = 1
+            point_google = (145, point_google[1] + 200)# second google acc
+    os.system(f"{ADB} shell input tap {point_confirm[0]} {point_confirm[1]}")
+
+
 
 # start script
-os.system(f"{ADB} shell input tap {point_take[0]} {point_take[1]}") # take daily gift                        # Inside the castle
-for i in range(3):# close ad (3 times close)
-    wait()        # close ad
-os.system(f"{ADB} shell input tap {point_lord[0]} {point_lord[1]}")
-os.system(f"{ADB} shell input tap {point_harvest[0]} {point_harvest[1]}")
-os.system(f"{ADB} shell input tap {point_use[0]} {point_use[1]}")
-for i in range(2):
-    wait()
-os.system(f"{ADB} shell input tap {point_map[0]} {point_map[1]}")
-time.sleep(5)
+for farm in range(8):
+    os.system(f"{ADB} shell input tap {point_take[0]} {point_take[1]}") # take daily gift                        # Inside the castle
+    for i in range(3):# close ad (3 times close)
+        wait()        # close ad
+    os.system(f"{ADB} shell input tap {point_lord[0]} {point_lord[1]}")
+    os.system(f"{ADB} shell input tap {point_harvest[0]} {point_harvest[1]}")
+    os.system(f"{ADB} shell input tap {point_use[0]} {point_use[1]}")
+    for i in range(2):
+        wait()
+    os.system(f"{ADB} shell input tap {point_map[0]} {point_map[1]}")
+    time.sleep(5)
 
-for i in range(3):
-    get_mine()
-
-
-
+    for i in range(3):
+        get_mine()
 
 
 
-os.system(f"{ADB} shell input tap {point_favorites[0]} {point_favorites[1]}")
-os.system(f"{ADB} shell input tap {point_elite[0]} {point_elite[1]}")
 
-check_color(point_elite_mine)
-if all(abs(a - t) < 5 for a, t in zip(color, (0, 0, 0, 255))): # line up, color = (XXX, XXX, XXX)
-    os.system(f"{ADB} shell input tap {point_elite_mine[0]} {point_elite_mine[1]}")
-    time.sleep(0.5)
-    os.system(f"{ADB} shell input tap {point_gather_elite[0]} {point_gather_elite[1]}")
-    os.system(f"{ADB} shell input tap {point_go[0]} {point_go[1]}")
-    print("point elite mine", point_elite_mine) # that and second print for debug (++230)
-    point_elite_mine = (point_elite_mine[0], point_elite_mine[1] + 230)
-    print("point elite mine", point_elite_mine)
-else:
-    print("some chemistry error")
+
+    os.system(f"{ADB} shell input tap {point_favorites[0]} {point_favorites[1]}")
+    os.system(f"{ADB} shell input tap {point_elite[0]} {point_elite[1]}")
+
+    check_color(point_elite_mine)
+    if all(abs(a - t) < 5 for a, t in zip(color, (34, 108, 137, 255))): # line up, color = (XXX, XXX, XXX)
+        os.system(f"{ADB} shell input tap {point_elite_mine[0]} {point_elite_mine[1]}")
+        time.sleep(0.5)
+        os.system(f"{ADB} shell input tap {point_gather_elite[0]} {point_gather_elite[1]}")
+        check_color(point_vip)# activate vip
+        if not color == (0, 132, 162, 255):# if castle => 19
+            os.system(f"{ADB} shell input tap {point_go[0]} {point_go[1]}")
+            point_elite_mine = (point_elite_mine[0], point_elite_mine[1] + 228)
+        else:
+            wait()
+            wait()
+    else:
+        print("some chemistry error")
+        os.system(f"{ADB} shell input tap {point_favourites_back[0]} {point_favourites_back[1]}")
+        os.system(f"{ADB} shell input tap {point_search[0]} {point_search[1]}")
+        get_mine()
+    second_farm()
--- times.py
+++ times.py
@@ -8,18 +8,20 @@ ADB = r"C:\Users\ignat\platform-tools\adb.exe"
 
 point_search_back = (790, 1890)
 point_close = (200, 1925)
-
+point_elite_mine = (380, 785)
+point_castle2 = (400, 1100)
 
 # start = time.perf_counter()
 # os.system(f"{ADB} shell input tap {point_close[0]} {point_close[1]}")
 # end = time.perf_counter()
 # print("1 click = ", end - start, "sec")  # 1,7 sec (0,7 just click)
-while True:
+for o in range(5):
     start = time.perf_counter()
     f = open("screen.png", "wb")  # should be done        with open("screen.png", "wb")
     subprocess.run([ADB, "exec-out", "screencap", "-p"], stdout = f)
     img = Image.open("screen.png")
-    color = img.getpixel((point_search_back[0], point_search_back[1]))
+    color = img.getpixel((900, 1455))
     end = time.perf_counter()
-    print("subprocess open screen + Image.open + get_pixel + ", end - start)
-    print("color of back = ", color, "sec")
+    print("subprocess open screen + Image.open + get_pixel + ", end - start, "sec")
+    print("color of green = ", "at", point_castle2, "cords", color,)
+    point_elite_mine = point_elite_mine[0], point_elite_mine[1] + 228
