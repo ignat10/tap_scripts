fcfe20a8121916c2d88306074aacaa1b592b2d6e
Ignat10 <ignat.shikh@gmail.com>
2025-08-17 20:05:26 +0200

    connect adb

---
+++ my_packages/adb_config.py
@@ -0,0 +1,39 @@
+from subprocess import run
+from time import sleep
+
+from pyperclip import paste
+
+IP = "192.168.0.1"
+
+def console(command: [str, str]) -> str:
+        return run(
+        command,
+        capture_output=True,
+        text=True,
+    ).stdout
+
+def connect_adb():
+    print("connecting adb")
+    devices = 0
+
+    while True:
+        output = console(['adb', "devices"])
+        print("stdout:", output)
+        devices = output.count("device") - 1 + output.count("offline")
+        print("devices:", devices)
+        match devices:
+            case 0:
+                clipboard = paste()
+                if clipboard.find(IP) != -1:
+                    print("connecting to device")
+                    run(["adb", "connect", clipboard])
+                else:
+                    print(f"copy your ip, now clipboard = {clipboard}")
+            case 1:
+                break
+            case _:
+                run(["adb", "disconnect"])
+                print("disconnect")
+        sleep(1)
+
+    print("connected to device")
