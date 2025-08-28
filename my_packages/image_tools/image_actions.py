31c618afccf952e4451582f00452180e62738686
Ignat10 <ignat.shikh@gmail.com>
2025-08-28 17:11:14 +0200

    screen actions

---
+++ my_packages/image_tools/image_actions.py
@@ -0,0 +1,17 @@
+from subprocess import run
+
+from PIL import Image
+
+def make_screen():
+    with open(f"screen.png", "wb") as f:
+        run(["adb", "exec-out", "screencap", "-p"], stdout = f)
+    return Image.open("screen.png")
+
+def get_pixel(screen, cords):
+    return screen.getpixel(cords)
+
+def check_color(point_checking: (int, int)):
+    return make_screen().getpixel((point_checking[0], point_checking[1]))
+
+def similar_color(color: (int, int, int, int), another_color: (int, int, int, int), tolerance: int) -> bool:
+    return all(abs(a - t < tolerance) for a, t in zip(another_color, color))
