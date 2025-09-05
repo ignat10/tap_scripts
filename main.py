3af2ef7594ed23d17cfba947be6b290c8aa916f7
Ignat10 <ignat.shikh@gmail.com>
2025-09-05 23:59:51 +0200

    remove *

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # local
 from my_packages.adb_tools.adb_config import connect_adb
-from my_packages.adb_tools.game_actions import *
+from my_packages.adb_tools.game_actions import farming
 
 def main():
     connect_adb()
