ab6350a9e5891e780584e53e4bd3e5517572dca8
Ignat10 <ignat.shikh@gmail.com>
2025-09-07 00:22:35 +0200

    check screen by cv2

--- my_packages/image_tools/screen_states.py
+++ my_packages/image_tools/screen_states.py
@@ -15,26 +15,38 @@ def main_menu():
 
 def mine_found():
     fullscreen = image_actions.make_screen_cv2()
-    part = cv2.imread(path("citi19"))
-    result = cv2.matchTemplate(fullscreen, part, cv2.TM_CCORR_NORMED)
-    return result[1]
+    city19 = cv2.imread(path("city19"))
+    search_menu = cv2.imread(path("search_mine_menu"))
+    is_city = True if cv2.minMaxLoc(cv2.matchTemplate(fullscreen, city19, cv2.TM_CCORR_NORMED))[1] > 0.95 else False
+    is_map = True if cv2.minMaxLoc(cv2.matchTemplate(fullscreen, search_menu, cv2.TM_CCORR_NORMED))[1] > 0.95 else False
+    print(f"city {is_city} || map {is_map}")
+    return is_map and not is_city
 
-# with open(f"screen.png", "wb") as f:
-#     subprocess.run(["adb", "exec-out", "screencap", "-p"], stdout = f)
-#
-# print("sleep")
-# time.sleep(4)
-#
-# with open("temp.png", "wb") as f:
-#     subprocess.run(["adb", "exec-out", "screencap", "-p"], stdout=f)
-#
-# main = cv2.imread("temp.png")
-# main = cv2.imread(path("mine_found"))
-# part = cv2.imread(path("city19"))
-# result = cv2.matchTemplate(main, part, cv2.TM_CCORR_NORMED)
-# _, max_val, _, max_loc = cv2.minMaxLoc(result)
-# print(max_val, max_loc)
-# screen = cv2.imread("screens/screen.png")
-# img = cv2.imread("screens/screen2.png")
-# print(cv2.PSNR(screen, img))
-print(mine_found())
+def visible_gather():
+    fullscreen = image_actions.make_screen_cv2()
+    gather = cv2.imread(path("gather"))
+    result = cv2.matchTemplate(fullscreen, gather, cv2.TM_CCORR_NORMED)
+    print(f"gather {cv2.minMaxLoc(result)[1]}")
+    is_gather = True if cv2.minMaxLoc(result)[1] > 0.90 else False
+    return is_gather
+
+if __name__ == "__screen_states__":
+    # with open(f"screen.png", "wb") as f:
+    #     subprocess.run(["adb", "exec-out", "screencap", "-p"], stdout = f)
+    #
+    # print("sleep")
+    # time.sleep(4)
+    #
+    # with open("temp.png", "wb") as f:
+    #     subprocess.run(["adb", "exec-out", "screencap", "-p"], stdout=f)
+    #
+    # main = cv2.imread("temp.png")
+    # main = cv2.imread(path("mine_found"))
+    # part = cv2.imread(path("city19"))
+    # result = cv2.matchTemplate(main, part, cv2.TM_CCORR_NORMED)
+    # _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    # print(max_val, max_loc)
+    # screen = cv2.imread("screens/screen.png")
+    # img = cv2.imread("screens/screen2.png")
+    # print(cv2.PSNR(screen, img))
+    print(visible_gather())
