1241b092cfc8610001dd4f50d1c235808f0d43da
Ignat10 <ignat.shikh@gmail.com>
2025-09-09 21:32:26 +0200

    add get coords and search x function

--- my_packages/image_tools/screen_states.py
+++ my_packages/image_tools/screen_states.py
@@ -1,20 +1,38 @@
 # libraries
-import cv2, time, subprocess
+import cv2, time, subprocess, os
 
+from my_packages.adb_tools.adb_config import connect_adb
 # packages
 from my_packages.image_tools import image_actions
 
 screens_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens\\"
 format = r".png"
+
+xs = sum(1 for name in os.listdir(screens_path) if "add" in name)
+
 def path(name: str):
     return screens_path + name + format
 
+def where_x(fullscreen, name: str):
+    part = cv2.imread(path(name))
+    match = cv2.matchTemplate(fullscreen, part, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
+    return max_loc if max_val > 0.9 else None
 
 def main_menu():
     return
 
+def get_coords_add():
+    fullscreen = image_actions.make_screen_cv2()
+    for i in range(xs):
+        coords = where_x(fullscreen, "add" + str(i))
+        if coords:
+            return coords
+    return None
+
 def mine_found():
     fullscreen = image_actions.make_screen_cv2()
+
     city19 = cv2.imread(path("city19"))
     search_menu = cv2.imread(path("search_mine_menu"))
     city_day = cv2.imread(path("city_day_19"))
