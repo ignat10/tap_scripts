cd5530c28b4240c41ac67378a4170a40668e1b89
Ignat10 <ignat.shikh@gmail.com>
2025-09-10 22:45:03 +0200

    return list

--- my_packages/image_tools/image_actions.py
+++ my_packages/image_tools/image_actions.py
@@ -15,15 +15,13 @@ def open_screen_PIL():
     make_screen()
     return Image.open(f"screen.png")
 
-def make_screen_cv2():
+def open_screen_cv2():
     make_screen()
     return cv2.imread("screen.png")
 
-def get_pixel(screen, cords):
-    return screen.getpixel(cords)
 
 def check_color(point_checking: (int, int)):
-    return make_screen().getpixel((point_checking[0], point_checking[1]))
+    return list[int](open_screen_PIL().getpixel((point_checking[0], point_checking[1])))
 
 def similar_color(color: (int, int, int, int), another_color: (int, int, int, int), tolerance: int) -> bool:
     return all(abs(a - t < tolerance) for a, t in zip(another_color, color))
