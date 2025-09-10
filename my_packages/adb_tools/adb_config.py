fc5f0425dcf1720e65942179b392c94367249c99
Ignat10 <ignat.shikh@gmail.com>
2025-09-10 18:41:30 +0200

    add list devices

--- my_packages/adb_tools/adb_config.py
+++ my_packages/adb_tools/adb_config.py
@@ -4,6 +4,7 @@ from time import sleep
 from pyperclip import paste
 
 IP = "192.168.0.1"
+_device_id = None
 
 def console(command: list[str]) -> str:
         return run(
@@ -12,17 +13,30 @@ def console(command: list[str]) -> str:
         text=True,
     ).stdout
 
+def list_devices() -> list[tuple]:
+    output = console(["adb, devices"])
+    lines = output.splitlines()
+    devices = []
+    for line in lines:
+        if line.strip():
+            name, status = line.split("\t")
+            devices.append((name, status))
+    return devices
+
 def connect_adb():
+    global _device_id
     print("connecting adb")
 
     while True:
-        output = console(['adb', "devices"])
-        print("stdout:", output)
-        online = output.count("device") - 1
-        offline = output.count("offline")
-        devices = online + offline
-        print(f"online: {online}, offline: {offline}:")
-        match devices:
+        devices = list_devices()
+        print("devices:", devices)
+        active_device = None
+        for serial, status in devices:
+            if status == "device":
+                active_device = serial
+                break
+
+        match active_device:
             case 0:
                 clipboard = paste()
                 if clipboard.find(IP) != -1:
@@ -30,10 +44,10 @@ def connect_adb():
                     run(["adb", "connect", clipboard])
                 else:
                     print(f"clipboard is '{clipboard}'\ncopy your ip")
-            case 1 if online:
+            case 1:
                 break
             case _:
-                run(["adb", "disconnect"])
+                pass
         sleep(1)
 
     print("connected to device")
