f7882cce3ec170481c1405349077d4eeea4da6c1
Ignat10 <ignat.shikh@gmail.com>
2025-09-14 21:02:54 +0200

    fix point step

--- my_packages/adb_tools/game_actions.py
+++ my_packages/adb_tools/game_actions.py
@@ -23,9 +23,11 @@ def wait():
     sleep(0.5)
     click(points["close"])
 
-def point_step(name: str, index):
-    point = points[name]
-    point[index] += STEPS[name]
+def point_step(name: str, index, times) -> list[int]:
+    point = list(points[name])
+    print(f"points[name]: {points[name]} point: {point} name: [{name}], times: {times}")
+    point[index] += STEPS[name] * times
+    print(f"return point: {point}")
     return point
 
 def close_add():
@@ -134,7 +136,7 @@ def get_elite():
         sleep(1)
         color = image_actions.check_color(points["elite_blue"])
         if color == COLORS["elite_blue"]:# color of blue
-            click(points["elite_blue"])
+            click(point_step("elite_blue", 1, which_blue))
             sleep(3)# too much but should work
             click(points["gather_elite"])
             sleep(1)
@@ -163,27 +165,29 @@ def second_farm():
     sleep(1)
     click(points["login"])
     sleep(2)
-    click(point_step("google", 1))
+    print(f"step gool, {which_google}")
+    click(point_step("google", 1, which_google))
     sleep(3)
-    click(point_step("castle", 1))
+    print(f"step acc: {which_acc}")
+    click(point_step("castle", 1, which_acc))
     sleep(1)
     click(points["confirm"])# go inside
     print("end second farm")
-    sleep(20)# I can make the still checking there
+    sleep(13)# I can make the still checking there
 
 def zeroing():
     global witch_mine
     witch_mine = 0
     # there can be zeroing lv (
 
-def set_which(acc_steps):
+def set_which(acc_steps: int):
     global which_google, which_acc
     for _ in range(acc_steps):
-        if which_acc < farms[which_google]:
+        if which_acc < farms[which_google] - 1:
             which_acc += 1
         else:
             which_google += 1
-            which_acc = 1
+            which_acc = 0
 
 def outside():
     get_mine()
