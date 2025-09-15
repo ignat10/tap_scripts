eb1d2c9ef1ad894270d2f4fcb8b463a534654742
Ignat10 <ignat.shikh@gmail.com>
2025-09-15 21:25:21 +0200

    diff paths

---
+++ my_packages/data/paths.py
@@ -0,0 +1,4 @@
+screens_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens\\"
+format = r".png"
+
+temp_screen_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\local\screen.png"
