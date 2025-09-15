fbe1f4a1bb5a94fa4d9ef14e82d9be7666a1a6ea
Ignat10 <ignat.shikh@gmail.com>
2025-09-15 21:26:38 +0200

    all screen folder system

--- my_packages/image_tools/screen_states.py
+++ my_packages/image_tools/screen_states.py
@@ -4,80 +4,94 @@ import cv2, os, time
 from my_packages.adb_tools.adb_config import connect_adb
 # packages
 from my_packages.image_tools import image_actions
+from my_packages.data.paths import screens_path, format
 
-screens_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens\\"
-format = r".png"
 
-def path(name: str):
-    return screens_path + name + format
+def path(folder_path: str, name: str):
+    return folder_path + name + format
 
 
 
 def loading():
+    folder_path = screens_path + r"main_menus\\"
     fullscreen = image_actions.open_screen_cv2()
-    for i in range(sum(1 for name in os.listdir(screens_path) if "main_menu" in name)):
-        origin = cv2.imread(path("main_menu" + str(i)))
+    for name in os.listdir(folder_path):
+        origin = cv2.imread(folder_path + name)
         result = cv2.PSNR(fullscreen, origin)
-        print(f"result: {result}")
-        if result > 10:
+        print(f"result [{name}]: {result}")
+        if result > 12:
             return False
     return True
 
 def main_menu():
+    folder_path = screens_path + r"main_menus\\"
     fullscreen = image_actions.open_screen_cv2()
-    for i in range(sum(1 for name in os.listdir(screens_path) if "main_menu" in name)):
-        part = cv2.imread(path("main_menu" + str(i)))
+
+    for name in os.listdir(folder_path):
+        part = cv2.imread(folder_path + name)
         result = cv2.PSNR(fullscreen, part)
         print(f"result: {result}")
         if result > 17:
             return True
+
     return False
 
-def where_x(fullscreen, name: str):
-    print(f"name = {name}")
-    part = cv2.imread(path(name))
+
+def where_x(fullscreen, x_path: str):
+    part = cv2.imread(x_path)
     match = cv2.matchTemplate(fullscreen, part, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
-    print(f"name: {name, min_val, max_val, min_loc, max_loc}")
+    print(f"name: {min_val, max_val, min_loc, max_loc}")
     return max_loc if max_val > 0.9 else None
 
 
 def get_coords_add():
-    xs = sum(1 for name in os.listdir(screens_path) if "x" in name)
-    print(f"xs = {xs}")
-    time.sleep(0.2)
+    folder_path = screens_path + r"xs\\"
     fullscreen = image_actions.open_screen_cv2()
-    time.sleep(0.2)
-    for i in range(xs):
-        coords = where_x(fullscreen, "x" + str(i))
+
+    for name in os.listdir(folder_path):
+        coords = where_x(fullscreen, folder_path + name)
         if coords:
-            print(f"found x{i} at {coords}")
+            print(f"found {name} at {coords}")
             return coords
     return None
 
-def mine_found():
+def mine_found() -> bool:
+    city = is_city()
+    menu = is_menu()
+
+    print(f"city {city} || map/menu {menu}")
+    return menu and not city
+
+def is_city() -> bool:
+    GAP = 0.9
+    folder_path = screens_path + r"map\cities\\"
+
+    return is_screen(folder_path, GAP)
+
+def is_menu() -> bool:
+    folder_path = screens_path + r"map\search_menus\\"
+    GAP = 0.9
+
+    return is_screen(folder_path, GAP)
+
+def is_screen(folder_path, GAP: float) -> bool:
     fullscreen = image_actions.open_screen_cv2()
 
-    city19 = cv2.imread(path("city0"))
-    search_menu = cv2.imread(path("search_mine_menu"))
-    city_day = cv2.imread(path("city1"))
+    for name in os.listdir(folder_path):
+        origin = cv2.imread(folder_path + name)
+        matched = cv2.matchTemplate(fullscreen, origin, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, _ = cv2.minMaxLoc(matched)
+        if max_val > GAP:
+            return True
 
-    float_city = cv2.minMaxLoc(cv2.matchTemplate(fullscreen, city19, cv2.TM_CCORR_NORMED))[1]
-    float_city_day = cv2.minMaxLoc(cv2.matchTemplate(fullscreen, city_day, cv2.TM_CCORR_NORMED))[1]
-    float_menu = cv2.minMaxLoc(cv2.matchTemplate(fullscreen, search_menu, cv2.TM_CCORR_NORMED))[1]
-
-    is_city = True if (float_city > 0.90) or (float_city_day > 0.95) else False
-    is_map = True if float_menu > 0.95 else False
-    print(f"city {float_city}, {is_city} || map {float_menu}, {is_map}")
-    return is_map and not is_city
+    return False
 
 def visible_gather():
-    fullscreen = image_actions.open_screen_cv2()
-    gather = cv2.imread(path("gather"))
-    result = cv2.matchTemplate(fullscreen, gather, cv2.TM_CCORR_NORMED)
-    print(f"gather {cv2.minMaxLoc(result)[1]}")
-    is_gather = True if cv2.minMaxLoc(result)[1] > 0.90 else False
-    return is_gather
+    folder_path = str(screens_path + r"map\\" + r"gather\\")
+    GAP = 0.8
+
+    return is_screen(folder_path, GAP)
 
 if __name__ == "__main__":
     print(cv2.PSNR(image_actions.open_screen_cv2(), cv2.imread(path("main_menu1"))))
