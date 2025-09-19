899f3549aa8e1844a979bcdf2972fdc017bee3ce
Ignat10 <ignat.shikh@gmail.com>
2025-09-19 23:28:00 +0200

    path dict from dir + pathlib

--- my_packages/data/paths.py
+++ my_packages/data/paths.py
@@ -1,4 +1,10 @@
-screens_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens\\"
-format = r".png"
+from pathlib import Path
+from os import listdir
 
-temp_screen_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\local\screen.png"
+screens = Path(r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens")
+
+screen_state_path = Path(r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\local\screen.png")
+path: dict = {}
+
+for name in listdir(screens):
+    path[name] = screens / name
