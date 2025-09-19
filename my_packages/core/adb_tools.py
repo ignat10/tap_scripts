016c0716529a939067480cfd73deb916756bd0b4
Ignat10 <ignat.shikh@gmail.com>
2025-09-19 23:23:02 +0200

    rm subprocess

--- my_packages/core/adb_tools.py
+++ my_packages/core/adb_tools.py
@@ -1,15 +1,16 @@
 from os import system
-from subprocess import run
 
 
 from my_packages.adb_tools.adb_config import get_device_name
-from my_packages.data.paths import temp_screen_path
+from my_packages.data.paths import screen_state_path
+
 
 device = get_device_name()
+
+
 def click(cords: (int, int)):
     system(f"adb -s {device} shell input tap {cords[0]} {cords[1]}")
 
 
 def make_screen():
-    with open(temp_screen_path, "wb") as f:
-        run(["adb", "-s", device, "exec-out", "screencap", "-p"], stdout = f)
+    system(f"adb -s {device} exec-out screencap -p > {screen_state_path}")
