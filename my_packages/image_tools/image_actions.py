5af200555a8091305b872e833857f481a3fadb3e
Ignat10 <ignat.shikh@gmail.com>
2025-09-29 21:01:06 +0200

    add temp screen

--- my_packages/image_tools/image_actions.py
+++ my_packages/image_tools/image_actions.py
@@ -1,12 +1,9 @@
 import cv2, os
 from PIL import Image
 
-from my_packages.adb_tools.adb_config import get_device_name
 from my_packages.data.paths import screen_state_path
 from my_packages.core.adb_tools import make_screen
 
-
-device = get_device_name()
 
 
 
@@ -20,23 +17,21 @@ def open_screen_cv2():
     return cv2.imread(screen_state_path)
 
 
-
-def check_color(point_checking: (int, int)):
+def check_color(point_checking: list[int]):
     return list[int](open_screen_pillow().getpixel((point_checking[0], point_checking[1])))
 
 
-def similar_color(color: (int, int, int, int), another_color: (int, int, int, int), tolerance: int) -> bool:
-    return all(abs(a - t < tolerance) for a, t in zip(another_color, color))
-
-
-
-def search_part(folder_path, gap: float):
-    fullscreen = open_screen_cv2()
+def search_part(folder_path, gap: float, fullscreen=None):
+    if fullscreen is None:
+        fullscreen = open_screen_cv2()
+    else:
+        fullscreen = cv2.imread(screen_state_path)
 
     for name in os.listdir(folder_path):
         origin = cv2.imread(folder_path / name)
         matched = cv2.matchTemplate(fullscreen, origin, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matched)
+        print(f"matched {name}: {max_val}/1.0")
         if max_val > gap:
             return max_loc
 
@@ -49,6 +44,7 @@ def is_fullscreen(folder_path, gap: int) -> bool:
     for name in os.listdir(folder_path):
         origin = cv2.imread(folder_path / name)
         matched = cv2.PSNR(fullscreen, origin)
+        print(f"similarity :{matched}/50")
         if matched > gap:
             return True
 
