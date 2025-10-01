0ca55e5d0f82ef1f40145c3f5e29e2110d060050
Ignat10 <ignat.shikh@gmail.com>
2025-10-01 22:27:41 +0200

    absolutely not working version

--- my_packages/adb_tools/game_actions.py
+++ my_packages/adb_tools/game_actions.py
@@ -1,13 +1,108 @@
 from time import sleep
 
 from my_packages.data.poco_coordinates import points, STEPS, COLORS
-from my_packages.data import farms
+from my_packages.data.farms import castles
 from my_packages.image_tools import image_actions, screen_states, get_coords
-from my_packages.utils import inputter
 from my_packages.core.adb_tools import click
 
-which_blue_MIA = 0
-which_blue_ = 0
+
+
+class Farm:
+    def __init__(self, number: int, name: str, google: int, account: int, lv: int, alliance: str):
+        self.number = number
+        self.name = name
+        self.google = google
+        self.account = account
+        self.lv = lv
+        self.alliance = alliance
+        self.mine_lv = 6
+        self.mine_type = 0
+        self.blue = 0
+
+    def outside(self):
+        sleep(3)
+        print("running outside")
+        for mine in range(4):
+            if mine == 2:
+                if self.get_elite():
+                    continue
+            self.get_mine()
+
+    def get_mine(self):  # to go to basic mine from the map
+        click(points["search"])
+        while True:
+            self.find_another_mine()
+            match screen_states.search_state():
+                case screen_states.Mine.FOUND_VISIBLE:
+                    print("gather is visible")
+                    break
+                case screen_states.Mine.FOUND_NOT_VISIBLE:  # if mine found but point gather is invisible
+                    print("gather is invisible")
+                    click(points["mine"])
+                    break
+                case screen_states.Mine.NOT_FOUND:  # if mine not found
+                    if self.mine_type < 4:
+                        print("second mine type")
+                        self.mine_type += 1
+                    else:
+                        print("less lv")
+                        self.mine_lv -= 1
+                        self.mine_type = 0
+                case screen_states.Mine.NOT_MAP:
+                    print("somehow I'm not at the map.\npanic")
+        wait_and_click(points["mine"])
+        sleep(2)
+        gather_mine()
+
+    def find_another_mine(self):  # to find another mine if not found
+        wait_and_click(point_step("mine_type", 0, self.mine_type))
+        repeat_click(points["minus"], 5)
+        repeat_click(points["plus"], self.mine_lv - 1)
+        repeat_click(points["go_mine"], 3)
+
+    def get_elite(self):
+        print("Elite")
+        match self.number:
+            case 0 | 0:
+                which_blue = self.blue
+                second_blue = False
+            case _:
+                which_blue = 0
+                second_blue = True
+        while True:
+            click(points["favorites"])
+            sleep(1)
+            click(points["alliance_elite"])
+            sleep(1)
+            color = image_actions.check_color(points["elite_blue"])
+            if color == COLORS["elite_blue"]:  # color of blue
+                click(point_step("elite_blue", 1, which_blue))
+                sleep(3)  # too much but should work
+                click(points["gather_elite"])
+                sleep(1)
+                click(points["go"])  # regularly I should be there
+                if second_blue:
+                    which_blue += 1
+                return True  # everything is alright I went to elite
+            else:
+                print("some chemistry error", color)
+                click(points["favourites_back"])
+                return False  # if there is no elites
+
+    def second_farm(self):
+        print("running second_farm")
+        wait_and_click(points["avatar"])
+        wait_and_click(points["account"])
+        wait_and_click(points["switch"])
+        wait_and_click(points["login"], 1)
+        print(f"step google, {self.google}")
+        wait_and_click(point_step("google", 1, self.google), 2)
+        print(f"step acc: {self.account}")
+        wait_and_click(point_step("castle", 1, self.account), 3)
+        wait_and_click(points["confirm"], 1)  # go inside
+        print("end second farm")
+
+
 
 
 def wait_and_click(coords: tuple[int, int], delay=0.5):
@@ -41,89 +136,10 @@ def close_add():
         click(coords)
 
 
-def find_another_mine(lv, mine_type):  # to find another mine if not found
-    wait_and_click(point_step("mine_type", 0, mine_type))
-    repeat_click(points["minus"], 5)
-    repeat_click(points["plus"], lv - 1)
-    repeat_click(points["go_mine"], 3)
-
-
 def gather_mine():
     wait_and_click(points["gather"])
     wait_and_click(points["go"])
     wait_and_click(points["back"])
-
-
-def get_mine(lv: int, mine_type: int):  # to go to basic mine from the map
-    click(points["search"])
-    while True:
-        find_another_mine(lv, mine_type)
-        match screen_states.search_state():
-            case screen_states.Mine.FOUND_VISIBLE:
-                print("gather is visible")
-                break
-            case screen_states.Mine.FOUND_NOT_VISIBLE:  # if mine found but point gather is invisible
-                print("gather is invisible")
-                click(points["mine"])
-                break
-            case screen_states.Mine.NOT_FOUND: # if mine not found
-                if mine_type < 4:
-                    print("second mine type")
-                    mine_type += 1
-                else:
-                    print("less lv")
-                    lv -= 1
-                    mine_type = 0
-            case screen_states.Mine.NOT_MAP:
-                print("somehow I'm not at the map.\npanic")
-    wait_and_click(points["mine"])
-    sleep(2)
-    gather_mine()
-    return lv, mine_type
-
-
-def get_elite(google: int, castle: int):
-    print("Elite")
-    match (google, castle):
-        case (0, 0) | (2, 1):
-            which_blue = 0
-            second_blue = False
-        case _:
-            which_blue = which_blue_MIA
-            second_blue = True
-    while True:
-        click(points["favorites"])
-        sleep(1)
-        click(points["alliance_elite"])
-        sleep(1)
-        color = image_actions.check_color(points["elite_blue"])
-        if color == COLORS["elite_blue"]:  # color of blue
-            click(point_step("elite_blue", 1, which_blue))
-            sleep(3)  # too much but should work
-            click(points["gather_elite"])
-            sleep(1)
-            click(points["go"])  # regularly I should be there
-            if second_blue:
-                which_blue += 1
-            return True  # everything is alright I went to elite
-        else:
-            print("some chemistry error", color)
-            click(points["favourites_back"])
-            return False  # if there is no elites
-
-
-def second_farm(google: int, castle: int):
-    print("running second_farm")
-    wait_and_click(points["avatar"])
-    wait_and_click(points["account"])
-    wait_and_click(points["switch"])
-    wait_and_click(points["login"], 1)
-    print(f"step google, {google}")
-    wait_and_click(point_step("google", 1, google), 2)
-    print(f"step acc: {castle}")
-    wait_and_click(point_step("castle", 1, castle), 3)
-    wait_and_click(points["confirm"], 1)  # go inside
-    print("end second farm")
 
 
 def loading():
@@ -154,27 +170,26 @@ def inside():
     print("finished inside")
 
 
-def outside(google: int, castle: int):
-    sleep(3)
-    print("running outside")
-    lv = 6  # level of mine
-    mine_type = 0
-    for mine in range(4):
-        if mine == 2:
-            if get_elite(google, castle):
-                continue
-        lv, mine_type = get_mine(lv, mine_type)
-
-
-def farm_castle(google: int, castle: int):
+def farm_castle(c):
     inside()
-    outside(google, castle)
-    second_farm(google, castle)
+    c.outside()
+    c.second_farm()
 
 
 def farming():
-    for google in range(inputter.farm_number(), len(farms)):
-        print(f"google: {google}")
-        for castle in range(farms[google]):
-            print(f"farming castle {castle}")
-            farm_castle(google, castle)
+    for c in castles:
+        farm_castle(c)
+
+
+# how to fix most likely due to a circular import
+
+# where to storage castles
+
+#different between self.fn || farm.fn
+
+# gow to minimalize connection between modules and max them kol-vo
+
+# fix get_elite excludes
+
+# where to put general fns
+
