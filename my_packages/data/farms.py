47cf04d9118a44d7a97ee24416d4e12f0e27be1e
Ignat10 <ignat.shikh@gmail.com>
2025-10-01 21:01:31 +0200

    add Farm class

---
+++ my_packages/data/farms.py
@@ -0,0 +1,8 @@
+class Farm:
+    def __init__(self, name, lv, alliance):
+        self.name = name
+        self.lv = lv
+        self.alliance = alliance
+        self.mine_lv = 6
+        self.mine_type = 0
+
