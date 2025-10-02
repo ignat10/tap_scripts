d53ea45f17a939db9fa58f84110a5e8bf2ac5f14
Ignat10 <ignat.shikh@gmail.com>
2025-10-02 22:48:56 +0200

    rewrite adb_config with import and using the core adb console functions

---
+++ my_packages/adb_helpers/adb_config.py
@@ -0,0 +1,23 @@
+from time import sleep
+from pyperclip import paste
+
+from my_packages.core.adb_console import find_device, connect_device
+
+IP = "192.168.0.1"
+
+def connect_adb():
+    print("connecting adb")
+    while True:
+        device = find_device()
+        if device is not None:
+            break
+        else:
+            clipboard = paste()
+            if clipboard.find(IP) != -1:
+                print(f"connecting to device '{clipboard}'")
+                connect_device(clipboard)
+            else:
+                print(f"clipboard is '{clipboard}'\ncopy your IP")
+        sleep(1)
+
+    print(f"connected to device '{device}'")
