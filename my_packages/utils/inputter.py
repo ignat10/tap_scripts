49a7750a2e0cda7caa2cac2b492c8133db6d6068
Ignat10 <ignat.shikh@gmail.com>
2025-10-02 22:39:12 +0200

    input google start number

--- my_packages/utils/inputter.py
+++ my_packages/utils/inputter.py
@@ -1,5 +1,5 @@
 def farm_number() -> int:
-    number = input("enter from which farm do we start: ")
+    number = input("enter from which google do we start: ")
 
     if number.isdigit():
         return int(number)
