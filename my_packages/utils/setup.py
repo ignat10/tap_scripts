f3c42839e4d3836f201b0f0562e6d664f0a4a5b6
Ignat10 <ignat.shikh@gmail.com>
2025-10-02 22:58:30 +0200

    castle's creating function

---
+++ my_packages/utils/setup.py
@@ -0,0 +1,12 @@
+from my_packages.data.farms import accounts, leo
+from my_packages.utils.inputter import farm_number
+from my_packages.adb_helpers.game_actions import Farm
+
+def make_castles() -> list[Farm]:
+    castles: list = []
+    number = 1
+    for google, count in enumerate(accounts[farm_number()::]):
+        for account in range(count):
+            castles.append(Farm(number, leo[0], google, account, leo[1], leo[2]))
+            number += 1
+    return castles
