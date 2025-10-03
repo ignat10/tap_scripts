5a5bbacd923831a2512f6690456e74be4a28e459
Ignat10 <ignat.shikh@gmail.com>
2025-10-03 20:04:32 +0200

    path: dict[str, list[Path]]

--- my_packages/data/paths.py
+++ my_packages/data/paths.py
@@ -1,10 +1,15 @@
 from os import listdir
+
 from pathlib import Path
 
 screens = Path(r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens")
 
 screen_state_path = Path(r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\local\screen.png")
-path: dict = {}
+path: dict[str, list[Path]] = {}
 
-for name in listdir(screens):
-    path[name] = screens / name
+for fol in listdir(screens):
+    path[fol] = []
+    for img in listdir(screens / fol):
+        path[fol].append(screens / fol / img)
+
+print(path)
