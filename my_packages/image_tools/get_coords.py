7da6c373f7ddb177f27527e4e5c966f7f456377d
Ignat10 <ignat.shikh@gmail.com>
2025-10-05 21:55:51 +0200

    add pathlib classtype

--- my_packages/image_tools/get_coords.py
+++ my_packages/image_tools/get_coords.py
@@ -1,7 +1,6 @@
-from my_packages.data.paths import path
 from my_packages.image_tools.image_actions import search_part
 
 
-def x():
-    coords = search_part(path["xs"], 0.9)
+def x() -> tuple[int, int] | None:
+    coords = search_part("xs", 0.9)
     return coords
--- my_packages/image_tools/image_actions.py
+++ my_packages/image_tools/image_actions.py
@@ -1,8 +1,8 @@
 import cv2
 from PIL import Image
 
-from my_packages.core.adb_console import make_screen
-from my_packages.data.paths import screen_state_path, path
+from my_packages.core.adb_utils import make_screen
+from my_packages.data.paths import screen_state_path, path, Path
 
 
 def _open_screen_pillow():
@@ -19,17 +19,17 @@ def check_color(point_checking: tuple[int, int]):
     return list[int](_open_screen_pillow().getpixel((point_checking[0], point_checking[1])))
 
 
-def search_part(folder_name, gap: float, fullscreen=None):
+def search_part(folder_name: str, gap: float, fullscreen=None):
     if fullscreen is None:
         fullscreen = _open_screen_cv2()
     else:
         fullscreen = cv2.imread(screen_state_path)
 
     for name in path[folder_name]:
-        origin = cv2.imread(str(name))
+        origin = cv2.imread(name)
         matched = cv2.matchTemplate(fullscreen, origin, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matched)
-        print(f"matched {name}: {max_val}/1.0")
+        print(f"matched {max_val}/1.0")
         if max_val > gap:
             return max_loc
     return None
