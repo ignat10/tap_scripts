9e9f73cdfd27ab8781d3ff6c9adb9e8817f1f8bd
Ignat10 <ignat.shikh@gmail.com>
2025-10-06 14:34:24 +0200

    connecting ADB device and saving data to imported object

--- my_packages/adb_helpers/adb_config.py
+++ my_packages/adb_helpers/adb_config.py
@@ -1,28 +1,27 @@
 from time import sleep
 import pyperclip
 
-from my_packages.core.adb_device import find_device, connect_device
+from my_packages.core.adb_device import device
 from my_packages.data.poco_coordinates import DEVICE_IP
 
 
 def connect_adb() -> None:
     print("connecting adb")
     while True:
-        device = find_device()
-        if device is not None:
+        device.find_device()
+        if device.device is not None:
             break
         else:
             clipboard = pyperclip.paste()
-            if clipboard.find(DEVICE_IP) != -1:
+            if DEVICE_IP in clipboard:
                 print(f"connecting to device '{clipboard}'")
-                if connect_device(clipboard):
+                device.connect_device(clipboard)
+                if device.is_connected:
                     continue
                 else:
                     print(f"error connecting to device '{clipboard}'\nreset clipboard")
-                    pyperclip.copy(None)
+                    pyperclip.copy("")
             else:
                 print(f"clipboard is '{clipboard}'\ncopy your IP")
         sleep(1)
-
-    print(f"connected to device '{device}'")
-    return None
+    print(f"connected to device '{device.device}'")
