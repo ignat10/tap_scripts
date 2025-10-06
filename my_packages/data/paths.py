1f3587177631ae6d9bf2b02071c1e1125eb571f1
Ignat10 <ignat.shikh@gmail.com>
2025-10-06 14:33:25 +0200

    fix paths

--- my_packages/data/paths.py
+++ my_packages/data/paths.py
@@ -1,15 +1,15 @@
-from os import listdir
+import os
 
-from pathlib import Path
 
-screens = Path(r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens")
+screens = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\image_tools\screens"
+screen_state_path = r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\local\screen.png"
+path: dict[str, list[str]] = {}
 
-screen_state_path = Path(r"D:\Documents\GitHub\PythonProject\tap_scripts\my_packages\local\screen.png")
-path: dict[str, list[Path]] = {}
-
-for fol in listdir(screens):
+for fol in os.listdir(screens):
+    folder_path = os.path.join(screens, fol)
     path[fol] = []
-    for img in listdir(screens / fol):
-        path[fol].append(screens / fol / img)
+    for img in os.listdir(folder_path):
+        screen_path = os.path.join(folder_path, img)
+        path[fol].append(screen_path)
 
 print(path)
