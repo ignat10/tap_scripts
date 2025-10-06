7417187b4cfc2a7c9a7e8bcd3be9279dc9eee8fb
Ignat10 <ignat.shikh@gmail.com>
2025-10-06 14:35:21 +0200

    class and methods to connect ADB device. Here object is created

--- my_packages/core/adb_device.py
+++ my_packages/core/adb_device.py
@@ -1,32 +1,32 @@
 from .adb_console import adb_run
 
-device = None
+
+class AdbDevice:
+    def __init__(self):
+        self.device = None
+        self.is_connected = False
 
 
-def find_device() -> str | None:
-    global device
-    output = adb_run("devices", capture_output=True, text=True)
-    if output is not None:
-        lines: list[str] = output.splitlines()
-        devices: list[tuple[str, str]] = []
-        for line in lines[1::]:
-            if line.strip():
-                name, status = line.split()
-                devices.append((name, status))
-        for serial, status in devices:
-            if status == "device":
-                device = serial
-                break
-    print(f"find adb devices: {device}")
-    return device
+    def find_device(self):
+        output = adb_run("devices", capture_output=True, text=True)
+        if output is not None:
+            lines: list[str] = output.splitlines()
+            devices: list[tuple[str, str]] = []
+            for line in lines[1::]:
+                if line.strip():
+                    name, status = line.split()
+                    devices.append((name, status))
+            for serial, status in devices:
+                if status == "device":
+                    self.device = serial
+                    break
+        print(f"find adb devices self.device: {self.device}")
 
 
-def connect_device(serial: str) -> bool:
-    output = adb_run(f"connect {serial}", capture_output=True, text=True)
-    is_success: bool = output.find(f"connected to {serial}") == 1
-    print(f"found {is_success} connected devs")
-    return is_success
+    def connect_device(self, dev: str) -> None:
+        output = adb_run(f"connect {dev}", capture_output=True, text=True)
+        success: bool = f"connected to {dev}" in output
+        print(f"success connection: {success}")
+        self.is_connected = success
 
-
-def get_device_name() -> str | None:
-    return device
+device = AdbDevice()
