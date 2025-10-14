080e1de71e62240d308490895d9717806bbfe878
Ignat10 <ignat.shikh@gmail.com>
2025-10-14 19:58:22 +0200

    everything is methods, +enumerate

--- my_packages/models/farming.py
+++ my_packages/models/farming.py
@@ -1,9 +1,22 @@
+from time import sleep
+
 from my_packages.adb_helpers.adb_config import connect_adb
 from my_packages.utils.setup import make_castles
+
 
 
 def farming():
     connect_adb()
     castles = make_castles()
-    for castle in castles:
-        castle.farm_castle()
+    for number, castle in enumerate(castles):
+        castle.inside()
+        castle.outside()
+        castle.second_farm(castles[number + 1])
+
+
+def seconds():
+    connect_adb()
+    castles = make_castles()
+    for number, castle in enumerate(castles):
+        castle.second_farm(castles[number + 1])
+        sleep(15)
