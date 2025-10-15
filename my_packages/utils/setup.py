318cd839b777c686d72032dc92bff7f204c14343
Ignat10 <ignat.shikh@gmail.com>
2025-10-15 21:43:50 +0200

    local imports

--- my_packages/utils/setup.py
+++ my_packages/utils/setup.py
@@ -1,9 +1,9 @@
 from my_packages.adb_helpers.game_actions import Farm
-from my_packages.data.farms import accounts, leo
-from my_packages.utils.inputter import farm_number
 
 
 def make_castles() -> list[Farm]:
+    from my_packages.data.farms import accounts, leo
+    from my_packages.utils.inputter import farm_number
     castles: list = []
     number = 1
     for google, count in enumerate(accounts[farm_number()::]):
