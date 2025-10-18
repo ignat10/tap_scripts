534d9bce92375222bf8c480735152c63f4b39210
Ignat10 <ignat.shikh@gmail.com>
2025-10-18 17:33:57 +0200

    add ndarray type

--- my_packages/loaders/image_loader.py
+++ my_packages/loaders/image_loader.py
@@ -1,7 +1,9 @@
 from cv2 import imread
+from numpy import ndarray
+
 from my_packages.data.paths import path
 
 
-def read_images() -> dict[str, list]:
+def read_images() -> dict[str, list[ndarray]]:
     images: dict[str, list] = {folder_name: list(imread(image_path) for image_path in image_paths) for folder_name, image_paths in path.image_paths.items()}
     return images
