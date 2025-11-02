71462fcc97e26c23bb1a0fb1a91cbc7448681772
Ignat10 <ignat.shikh@gmail.com>
2025-11-02 19:09:37 +0100

    better generator

--- my_packages/data/paths.py
+++ my_packages/data/paths.py
@@ -11,9 +11,21 @@ class Path:
         self.cutted_screen = os.path.join(self._LOCAL_DIR, 'cutted_screen.png')
         self.farms_sheet_path = os.path.join(self._DATA_DIR, 'WAO_farms_data.xlsx')
         self.folder_names: frozenset[str] = frozenset(os.listdir(self._SCREENS_DIR))
-        self.folder_paths: dict[str, str] = {folder_name: os.path.join(self._SCREENS_DIR, folder_name) for folder_name in self.folder_names}
-        self.image_names: dict[str, frozenset[str]] = {folder_name: frozenset(os.listdir(folder_path)) for folder_name, folder_path in self.folder_paths.items()}
-        self.image_paths: dict[str, frozenset[str]] = {folder_name: frozenset(os.path.join(folder_path, image_name) for image_name in self.image_names[folder_name]) for folder_name, folder_path in self.folder_paths.items()}
+        self.folder_paths: dict[str, str] = {
+            folder_name: os.path.join(self._SCREENS_DIR, folder_name)
+            for folder_name in self.folder_names
+        }
+        self.image_names: dict[str, frozenset[str]] = {
+            folder_name: frozenset(os.listdir(folder_path))
+            for folder_name, folder_path in self.folder_paths.items()
+        }
+        self.image_paths: dict[str, frozenset[str]] = {
+            folder_name: frozenset(
+                os.path.join(folder_path, image_name)
+                for image_name in self.image_names[folder_name]
+            )
+            for folder_name, folder_path in self.folder_paths.items()
+        }
 
 
 path = Path()
--- my_packages/loaders/image_loader.py
+++ my_packages/loaders/image_loader.py
@@ -5,5 +5,10 @@ from my_packages.data.paths import path
 
 
 def read_images() -> dict[str, list[ndarray]]:
-    images: dict[str, list] = {folder_name: list(cvtColor(imread(image_path), COLOR_BGR2GRAY) for image_path in image_paths) for folder_name, image_paths in path.image_paths.items()}
-    return images
+    return {
+        folder_name: list(
+            cvtColor(imread(image_path), COLOR_BGR2GRAY)
+            for image_path in image_paths
+        )
+        for folder_name, image_paths in path.image_paths.items()
+    }
