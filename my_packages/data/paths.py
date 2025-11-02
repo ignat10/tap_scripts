7b764c229475cb921b95a9cc1654e4e14f12764b
Ignat10 <ignat.shikh@gmail.com>
2025-11-02 19:38:52 +0100

    image_paths.to_dict()

--- my_packages/data/paths.py
+++ my_packages/data/paths.py
@@ -19,11 +19,11 @@ class Path:
             folder_name: frozenset(os.listdir(folder_path))
             for folder_name, folder_path in self.folder_paths.items()
         }
-        self.image_paths: dict[str, frozenset[str]] = {
-            folder_name: frozenset(
-                os.path.join(folder_path, image_name)
+        self.image_paths: dict[str: dict[str: str]] = {
+            folder_name: {
+                image_name: os.path.join(folder_path, image_name)
                 for image_name in self.image_names[folder_name]
-            )
+            }
             for folder_name, folder_path in self.folder_paths.items()
         }
 
--- my_packages/loaders/image_loader.py
+++ my_packages/loaders/image_loader.py
@@ -4,11 +4,11 @@ from numpy import ndarray
 from my_packages.data.paths import path
 
 
-def read_images() -> dict[str, list[ndarray]]:
+def read_images() -> dict[str: dict[str: ndarray]]:
     return {
-        folder_name: list(
-            cvtColor(imread(image_path), COLOR_BGR2GRAY)
-            for image_path in image_paths
-        )
+        folder_name: {
+            image_name: cvtColor(imread(image_path), COLOR_BGR2GRAY)
+            for image_name, image_path in image_paths.items()
+        }
         for folder_name, image_paths in path.image_paths.items()
     }
