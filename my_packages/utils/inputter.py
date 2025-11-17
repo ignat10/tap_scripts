c61056ceff45f4f9de81c3fd0494bbb129aa3800
Ignat10 <ignat.shikh@gmail.com>
2025-11-17 22:16:53 +0100

    safe input

--- my_packages/utils/inputter.py
+++ my_packages/utils/inputter.py
@@ -1,7 +1,7 @@
-def farm_number() -> int:
-    number = input("enter from which google do we start: ")
-
-    if number.isdigit():
-        return int(number)
-    else:
-        return 0
+def farm_number(base_val: int) -> int:
+    
+    value = input("enter from which google do we start: ")
+    try:
+        return int(value)
+    except ValueError:
+        return base_val
