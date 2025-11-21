a6f702587d34e1b482ece85ad497b39a1f1c1b05
Ignat10 <ignat.shikh@gmail.com>
2025-11-21 19:24:43 +0100

    add Folder enum

--- my_packages/image_tools/image_manager.py
+++ my_packages/image_tools/image_manager.py
@@ -1,59 +1,71 @@
-import os.path
-
+from os import path, listdir
+from enum import Enum
 from numpy import ndarray
 
 
-_BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # packages
-_SCREENS_DIR = os.path.join(_BASE_DIR, 'image_tools', 'screens')
-_DATA_DIR = os.path.join(_BASE_DIR, 'data')
-_LOCAL_DIR = os.path.join(_BASE_DIR, 'local')
-screen_state_path = os.path.join(_LOCAL_DIR, 'screen.png')
-cutted_screen = os.path.join(_LOCAL_DIR, 'cutted_screen.png')
-farms_sheet_path = os.path.join(_DATA_DIR, 'WAO_farms_data.xlsx')
+class Folders(Enum):
+    ADS = "ads"
+    AVATARS = "avatars"
+    BLUE = "blue"
+    CITIES = "cities"
+    FAVOURITES = "favourites"
+    GATHER = "gather"
+    MAIN_MENUS = "main_menus"
+    SEARCH_BAR = "search_menus"
+    XS = "xs"
 
-folder_names: frozenset[str] = frozenset(os.listdir(_SCREENS_DIR))
+_BASE_DIR = path.dirname(path.dirname(__file__))  # packages
+_SCREENS_DIR = path.join(_BASE_DIR, 'image_tools', 'screens')
+_DATA_DIR = path.join(_BASE_DIR, 'data')
+_LOCAL_DIR = path.join(_BASE_DIR, 'local')
+screen_state_path = path.join(_LOCAL_DIR, 'screen.png')
+cut_screen = path.join(_LOCAL_DIR, 'cutted_screen.png')
+farms_sheet_path = path.join(_DATA_DIR, 'WAO_farms_data.xlsx')
+folder_names: set[str] = set(listdir(_SCREENS_DIR))
 
-folder_paths: dict[str, str] = {
-    folder_name: os.path.join(_SCREENS_DIR, folder_name)
-    for folder_name in folder_names
+
+folders_enum: set[str] = {item.value for item in Folders}
+assert folder_names == folders_enum, f"expected {folders_enum - folder_names}, found {folder_names - folders_enum}"
+
+THRESHOLDS: dict[Folders, float] = {
+    Folders.ADS: 0.9,
+    Folders.AVATARS: 0.8,
+    Folders.BLUE: 0.8,
+    Folders.CITIES: 0.8,
+    Folders.FAVOURITES: 0.8,
+    Folders.GATHER: 0.8,
+    Folders.MAIN_MENUS: 0.9,
+    Folders.SEARCH_BAR: 0.9,
+    Folders.XS: 0.8,
 }
 
-image_names: dict[str, frozenset[str]] = {
-    folder_name: frozenset(os.listdir(folder_path))
-    for folder_name, folder_path in folder_paths.items()
+folder_paths: dict[Folders, str] = {
+    folder: path.join(_SCREENS_DIR, folder.value)
+    for folder in Folders
 }
 
-image_paths: dict[str, dict[str, str]] = {
-    folder_name: {
-        image_name: os.path.join(folder_path, image_name)
-        for image_name in image_names[folder_name]
+
+image_names: dict[Folders, frozenset[str]] = {
+    folder: frozenset(listdir(folder_path))
+    for folder, folder_path in folder_paths.items()
+}
+
+image_paths: dict[Folders, dict[str, str]] = {
+    folder: {
+        image_name: path.join(folder_path, image_name)
+        for image_name in image_names[folder]
     }
-    for folder_name, folder_path in folder_paths.items()
+    for folder, folder_path in folder_paths.items()
 }
 
 
-def read_images() -> dict[str, dict[str, ndarray]]:
+def read_images() -> dict[Folders, dict[str, ndarray]]:
     from cv2 import imread, cvtColor, COLOR_BGR2GRAY
 
     return {
-        folder_name: {
+        folder: {
             image_name: cvtColor(imread(image_path), COLOR_BGR2GRAY)
             for image_name, image_path in image_paths.items()
         }
-        for folder_name, image_paths in image_paths.items()
+        for folder, image_paths in image_paths.items()
     }
-
-
-def get_image_gaps() -> dict[str, float]:
-    gaps: dict[str, float] = {
-        folder_name: 0.9
-        for folder_name in folder_names
-    }
-    gaps.update({
-        "ads": 0.6,
-        "blue": 0.8,
-        "cities": 0.6,
-        "gather": 0.6,
-        "main_menus": 0.8,
-    })
-    return gaps
