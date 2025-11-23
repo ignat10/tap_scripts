2d73223ce369e5afbfc8f33494f0e22ea2666f42
Ignat10 <ignat.shikh@gmail.com>
2025-11-23 10:23:07 +0100

    temp to pull

--- my_packages/image_tools/image_manager.py
+++ my_packages/image_tools/image_manager.py
@@ -7,7 +7,6 @@ from cv2 import imread, cvtColor, COLOR_BGR2GRAY
 
 class Folders(Enum):
     ADS = "ads"
-    AVATARS = "avatars"
     BLUE = "blue"
     CITIES = "cities"
     FAVOURITES = "favourites"
@@ -16,11 +15,12 @@ class Folders(Enum):
     MAIN_MENUS = "main_menus"
     SEARCH_BAR = "search_menus"
     XS = "xs"
+    LEO = path.join("avatars", "leo")
+    LORD = path.join("avatars", "lord")
 
 
 THRESHOLDS: dict[Folders, float] = {
     Folders.ADS: 0.9,
-    Folders.AVATARS: 0.8,
     Folders.BLUE: 0.8,
     Folders.CITIES: 0.8,
     Folders.FAVOURITES: 0.8,
@@ -29,29 +29,31 @@ THRESHOLDS: dict[Folders, float] = {
     Folders.MAIN_MENUS: 0.9,
     Folders.SEARCH_BAR: 0.9,
     Folders.XS: 0.8,
+
+    Folders.LEO: 0.8,
+    Folders.LORD: 0.8,
 }
 
 
 _BASE_DIR = path.dirname(path.dirname(__file__))  # packages
-_SCREENS_DIR = path.join(_BASE_DIR, 'image_tools', 'screens')
 _DATA_DIR = path.join(_BASE_DIR, 'data')
+_TEMPLATES_DIR = path.join(_DATA_DIR, 'templates')
 _LOCAL_DIR = path.join(_BASE_DIR, 'local')
 screen_state_path = path.join(_LOCAL_DIR, 'screen.png')
-cut_screen = path.join(_LOCAL_DIR, 'cutted_screen.png')
+cut_screen = path.join(_LOCAL_DIR, 'cut_screen.png')
 farms_sheet_path = path.join(_DATA_DIR, 'WAO_farms_data.xlsx')
 
-folder_names: set[str] = set(listdir(_SCREENS_DIR))
+folder_names: set[str] = set(listdir(_TEMPLATES_DIR))
 folders_enum: set[str] = {item.value for item in Folders}
-assert folder_names == folders_enum, f"expected {folders_enum - folder_names}, found {folder_names - folders_enum}"
+threshold_names: set[str] = {folder.value for folder in THRESHOLDS}
 
-thresholds_names: set[str] = {folder.value for folder in THRESHOLDS}
-assert folder_names == thresholds_names, f"expected {folders_enum - thresholds_names}, found {thresholds_names - folders_enum}"
+# assert folder_names == folders_enum, f"expected {folders_enum - folder_names}, found {folder_names - folders_enum}"
+# assert folder_names == threshold_names, f"expected {folders_enum - threshold_names}, found {threshold_names - folders_enum}"
 
 folder_paths: dict[Folders, str] = {
-    folder: path.join(_SCREENS_DIR, folder.value)
+    folder: path.join(_TEMPLATES_DIR, folder.value)
     for folder in Folders
 }
-
 
 image_names: dict[Folders, frozenset[str]] = {
     folder: frozenset(listdir(folder_path))
@@ -74,3 +76,15 @@ images: dict[Folders, dict[str, ndarray]] = {
     }
     for folder, image_paths in image_paths.items()
 }
+
+
+def get_paths(dir: str = _TEMPLATES_DIR):
+    return {
+        item: 
+        get_paths(full)
+        if path.isdir((full := path.join(dir, item)))
+        else full
+        for item in listdir(dir)
+    }
+p = get_paths()
+print(p)
