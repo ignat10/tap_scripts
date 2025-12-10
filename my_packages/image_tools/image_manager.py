8eb333642a15033cc5e27bc41b6c109e7ff35a29
Ignat10 <ignat.shikh@gmail.com>
2025-12-10 17:49:08 +0100

    Template class

--- my_packages/image_tools/image_manager.py
+++ my_packages/image_tools/image_manager.py
@@ -4,58 +4,42 @@ from numpy import ndarray
 from cv2 import imread, cvtColor, COLOR_BGR2GRAY
 
 
-_BASE_DIR = path.dirname(path.dirname(__file__))  # packages
-_DATA_DIR = path.join(_BASE_DIR, 'data')
-_TEMPLATES_DIR = path.join(_DATA_DIR, 'templates')
-_LOCAL_DIR = path.join(_BASE_DIR, 'local')
-screen_state_path = path.join(_LOCAL_DIR, 'screen.png')
-cut_screen = path.join(_LOCAL_DIR, 'cut_screen.png')
-farms_sheet_path = path.join(_DATA_DIR, 'WAO_farms_data.xlsx')
+from ..data.paths import TEMPLATES_DIR
 
 
+class Template:
+    def __init__(self, relative_path: str, threshold: float, coords: tuple[int, int] | None = None):
+        self.images = None
+        self.path = relative_path
+        self.threshold = threshold
+        self.coords = coords
+
+    def __call__(self):
+        if self.images is None:
+            self.images: list[ndarray] = [
+                cvtColor(imread(path.join(TEMPLATES_DIR, self.path, file_name)), COLOR_BGR2GRAY)
+                for file_name in listdir(path.join(TEMPLATES_DIR, self.path))
+            ]
+        return self.images
+    
 
 class Templates(Enum):
-    ADS = "ads"
-    BLUE = "blue"
-    BOOK = "book"
-    CITIES = "cities"
-    FAVOURITES = "favourites"
-    GATHER = "gather"
-    LOAD = "load"
-    MAIN_MENUS = "main_menus"
-    SEARCH_BAR = "search_menus"
-    XS = "xs"
-
-    LEO = path.join("avatars", "leo")
-    LORD = path.join("avatars", "lord")
-
-    def __call__(self) -> str:
-        return path.join(_TEMPLATES_DIR, self.value)
+    ADS = Template("ads", 0.9)
+    BLUE = Template("blue", 0.8)
+    BOOK = Template("book", 0.8, coords=(88, 2069))
+    CITIES = Template("cities", 0.8)
+    FAVOURITES = Template("favourites", 0.8)
+    GATHER = Template("gather", 0.8)
+    LOAD = Template("load", 0.9)
+    MAIN_MENUS = Template("main_menus", 0.9)
+    SEARCH_BAR = Template("search_menus", 0.9)
+    XS = Template("xs", 0.8)
+    LEO = Template(path.join("avatars", "leo"), 0.8)
+    LORD = Template(path.join("avatars", "lord"), 0.8)
 
 
-THRESHOLDS: dict[Templates, float] = {
-    Templates.ADS: 0.9,
-    Templates.BLUE: 0.8,
-    Templates.BOOK: 0.8,
-    Templates.CITIES: 0.8,
-    Templates.FAVOURITES: 0.8,
-    Templates.GATHER: 0.8,
-    Templates.LOAD: 0.9,
-    Templates.MAIN_MENUS: 0.9,
-    Templates.SEARCH_BAR: 0.9,
-    Templates.XS: 0.8,
-
-    Templates.LEO: 0.8,
-    Templates.LORD: 0.8,
-}
-
-
-COORDS: dict[Templates, tuple[int, int]] = {
-    Templates.BOOK: (88, 2069),
-}
-
-def get_paths(local_path: str = "") -> set[str] | None:
-    full_path = path.join(_TEMPLATES_DIR, local_path)
+def get_paths(local_path: str = "") -> set[str]:
+    full_path = path.join(TEMPLATES_DIR, local_path)
     tree = set()
 
     for item in listdir(full_path):
@@ -68,25 +52,7 @@ def get_paths(local_path: str = "") -> set[str] | None:
     return tree
     
 
+listdir_paths: set[str] = get_paths()
+Templates_enum: set[str] = {item.value.path for item in Templates}
 
-folder_names: set[str] = get_paths()
-Templates_enum: set[str] = {item.value for item in Templates}
-threshold_names: set[str] = {folder.value for folder in THRESHOLDS}
-
-assert folder_names == Templates_enum, f"expected {Templates_enum - folder_names}, found {folder_names - Templates_enum}"
-assert folder_names == threshold_names, f"expected {Templates_enum - threshold_names}, found {threshold_names - Templates_enum}"
-
-
-images: dict[Templates, dict[str, ndarray]] = {}
-
-def get_images(template: Templates) -> dict[str, ndarray]:
-    global images
-    if template not in images:
-        templates_path = path.join(_TEMPLATES_DIR, template.value)
-
-        images[template] = {
-            file_name: cvtColor(imread(path.join(templates_path, file_name)), COLOR_BGR2GRAY)
-            for file_name in listdir(templates_path)
-        }
-
-    return images[template]
+assert listdir_paths == Templates_enum, f"expected {Templates_enum - listdir_paths}, found {listdir_paths - Templates_enum}"
