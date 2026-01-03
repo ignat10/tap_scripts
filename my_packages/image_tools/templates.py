882179c72787581b469452aaedb63ec1dc62902b
Ignat10 <Ignat.shikh@gmail.com>
2026-01-03 01:03:36 +0100

    script to find a template location

--- my_packages/image_tools/templates.py
+++ my_packages/image_tools/templates.py
@@ -7,7 +7,7 @@ from .template_manager import Template
 
 ADS = Template(Path("ads"), 0.9)
 BLUE = Template(Path("blue"), 0.8)
-BOOK = Template(Path("book"), 0.8, coords=(88, 2069))
+BOOK = Template(Path("book"), 0.8, coords=(37, 2022))
 CITIES = Template(Path("cities"), 0.8)
 FAVOURITES = Template(Path("favourites"), 0.8)
 GATHER = Template(Path("gather"), 0.8)
