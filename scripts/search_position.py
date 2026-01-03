e65a322bc2b7c1786ca6a9ad932f4c14b14fd5ba
Ignat10 <Ignat.shikh@gmail.com>
2026-01-03 01:03:36 +0100

    script to find a template location

--- scripts/search_position.py
+++ scripts/search_position.py
@@ -1,28 +1,41 @@
-from cv2 import imread, imwrite, cvtColor, COLOR_BGR2GRAY, quality
-from numpy import array, ndarray
+from cv2 import imread, imwrite, cvtColor, COLOR_BGR2GRAY
+from numpy import ndarray
+from cv2.quality import QualitySSIM_compute as ssim
 
+from my_packages.image_tools import templates
 
-from my_packages.image_tools.template_manager import Templates
-
-
-ssim = quality.QualitySSIM_compute
+corter = (152, 2000)
+radius = 150
+template = templates.BOOK
 
 
 screen = cvtColor(imread("screen.png"), COLOR_BGR2GRAY)
-template = Templates.BOOK.value
-image = template.get_images().values().__iter__().__next__()
+image = template.get_images().__next__()
 
-corter = (570, 1280)
 X = corter[0]
 Y = corter[1]
-radius = 50
 
-matrix = array([
-    [
-        round(ssim(image, template.crop_screen(screen))[0][0], 2)
-        for x in range(X, X + radius)
-    ]
-    for y in range(Y, Y + radius)
-])
 
-print(matrix)
+def crop_screen(screen: ndarray, image: ndarray, corner: tuple[int, int]) -> ndarray:
+    y, x = image.shape
+    opposite_corner = (corner[0] + x, corner[1] + y)
+    crop_screen = screen[corner[1]:opposite_corner[1], corner[0]:opposite_corner[0]]
+    return crop_screen
+
+matrix = []
+for y in range(Y - radius, Y + radius):
+    row = []
+    for x in range(X - radius, X + radius):
+        crop = crop_screen(screen, image, (x, y))
+        similarity = round(ssim(crop, image)[0][0], 2)
+        row.append(((x, y), similarity))
+    matrix.append(row)
+
+
+max = (0,0), 0.0
+for y in matrix:
+    for x in y:
+        if x[1] > max[1]:
+            max = x
+
+print("Max similarity:", max)
