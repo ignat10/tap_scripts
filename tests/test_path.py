84b70698b4aa86c3ca31d8417a0fed7d4f93a111
Ignat10 <Ignat.shikh@gmail.com>
2026-01-04 15:47:52 +0100

    working test to check is real paths matches templates path

--- tests/test_path.py
+++ tests/test_path.py
@@ -3,31 +3,40 @@ import pytest
 from pathlib import Path
 
 from my_packages.data.paths import TEMPLATES_DIR
-from my_packages.image_tools.template_manager import Templates
+from my_packages.image_tools import templates
 
 
 
-def get_paths(full_path: Path = Path(TEMPLATES_DIR)):
+def get_listdir_paths(full_path: Path = Path(TEMPLATES_DIR)):
     listdir_items = full_path.iterdir()
 
     result = set()
     for item in listdir_items:
         if item.is_dir():
-            result.update(get_paths(item))
+            result.update(get_listdir_paths(item))
         else:
             result.add(full_path.relative_to(TEMPLATES_DIR))
     return result
 
 
 @pytest.fixture
-def get_paths_fixture():
-    return get_paths()
+def get_listdir_paths_fixture():
+    return get_listdir_paths()
 
 
-def test_templates_enum_matches_dirs(get_paths_fixture):
-    listdir_paths: set[Path] = get_paths_fixture
-    Templates_enum: set[Path] = {item.value.path for item in Templates}
+@pytest.fixture
+def get_templates_path_fixture():
+    return {
+        template.path
+        for attribute in dir(templates)
+        if isinstance((template := getattr(templates, attribute)), templates.Template)
+        }
 
-    assert listdir_paths == Templates_enum, (
-        f"expected {Templates_enum - listdir_paths}, \nfound {listdir_paths - Templates_enum}"
-    )
+
+def test_templates_enum_matches_dirs(
+        get_listdir_paths_fixture,
+        get_templates_path_fixture
+        ):
+    listdir_paths: set[Path] = get_listdir_paths_fixture
+    templates_path: set[Path] = get_templates_path_fixture
+    assert listdir_paths == templates_path
