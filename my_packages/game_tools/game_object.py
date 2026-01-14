35c510584dea9a934e209405f7c6bea0e94e7254
Ignat10 <Ignat.shikh@gmail.com>
2026-01-15 00:01:26 +0100

    better overload section

--- my_packages/game_tools/game_object.py
+++ my_packages/game_tools/game_object.py
@@ -28,48 +28,16 @@ class GameObject:
             path: str | None = None,
             threshold: float | None = None, 
     ) -> None:
-        if coords is not None:
-            self.coords: tuple[int, int] = tuple(coords)
-        if delta is not None:
-            self.delta: tuple[int, Axis] = (delta[0], Axis[delta[1]])
-        if path is not None:
-            self.path: Path = Path(path)
-        if threshold is not None:
-            self.threshold = threshold
+        self.coords: tuple[int, int] | None = (tuple(coords) if coords is not None else None)
+        self.delta: tuple[int, Axis] | None = ((delta[0], Axis[delta[1]]) if delta is not None else None)
+        self.path: Path | None = (Path(path) if path is not None else None)
+        self.threshold: float | None = (float(threshold) if threshold is not None else None)
         self._cache: dict[str, ndarray] = {}
-
-    @property
-    def images(self) -> Iterator[ndarray]:
-        for file_path in (TEMPLATES_DIR / self.path).iterdir():
-            file_name = file_path.name
-            if file_name not in self._cache:
-                self._cache[file_name] = cvtColor(imread(file_path), COLOR_BGR2GRAY)
-            yield self._cache[file_name]
-
-    def _compare_loop(
-        self,
-        screen: ndarray,
-        compare_method: Callable[[ndarray, ndarray], tuple[float, tuple[int, int] | None]]
-        ) -> bool | tuple[int, int]:
-        threshold = self.threshold
-        for image in self.images:
-            similarity, coords = compare_method(screen, image)
-            if similarity >= threshold:
-                return coords or True
-        return False
-
-    def _crop_screen(self, screen: ndarray, coords: tuple[int, int]) -> ndarray:
-        if coords is None:
-            return screen
-        corner = coords
-        y, x = next(self.images).shape
-        opposite_corner = (corner[0] + x, corner[1] + y)
-        crop_screen = screen[corner[1]:opposite_corner[1], corner[0]:opposite_corner[0]]
-        return crop_screen
 
     @staticmethod
     def _step(func):
         def wrapper(self: Self, steps: int=0, *args, **kwargs) -> bool | tuple[int, int]:
+            assert self.coords is not None, f"try to make step for {self.path} without coords"
             if steps == 0:
                 moved_coords = self.coords
             else:
@@ -81,13 +49,42 @@ class GameObject:
                 moved_coords = tuple(lst)
             return func(self, coords=moved_coords, *args, **kwargs)
         return wrapper
+    
+    @property
+    def images(self) -> Iterator[ndarray]:
+        for file_path in (TEMPLATES_DIR / self.path).iterdir():
+            file_name = file_path.name
+            if file_name not in self._cache:
+                self._cache[file_name] = cvtColor(imread(file_path), COLOR_BGR2GRAY)
+            yield self._cache[file_name]
 
+    @overload
+    def _compare_loop(self, screen: ndarray, compare_method: Callable[[ndarray, ndarray], tuple[float, None]]) -> bool: ...
+    @overload
+    def _compare_loop(self, screen: ndarray, compare_method: Callable[[ndarray, ndarray], tuple[float, tuple[int, int]]]) -> tuple[int, int] | None: ...
     @overload
     def click(self, *, delay: float=0.0, steps: int=0, repeat: int=1) -> None: ...
     @overload
     def find_and_click(self, *, do_screen: bool=True, base_object: Self | None = None) -> bool | tuple[int, int]: ...
     @overload
     def compare_part(self, *, do_screen: bool=True, steps: int=0) -> bool: ...
+
+    def _compare_loop(self, screen, compare_method):
+        threshold = self.threshold
+        for image in self.images:
+            similarity, coords = compare_method(screen, image)
+            if similarity >= threshold:
+                return coords or True
+        return None
+
+    def _crop_screen(self, screen: ndarray, coords: tuple[int, int]) -> ndarray:
+        if coords is None:
+            return screen
+        corner = coords
+        y, x = next(self.images).shape
+        opposite_corner = (corner[0] + x, corner[1] + y)
+        crop_screen = screen[corner[1]:opposite_corner[1], corner[0]:opposite_corner[0]]
+        return crop_screen
 
     @_step
     def click(
@@ -101,17 +98,12 @@ class GameObject:
             actions.input_tap(coords)
 
     @screen_manager.with_screen
-    def find_and_click(self, screen: ndarray, base_object: Self) -> None:
-        coords = self._compare_loop(screen, compare_methods.match_template) or base_object
+    def find_and_click(self, screen, base_object) -> None:
+        coords = self._compare_loop(screen, compare_methods.match_template) or base_object.coords
         actions.input_tap(coords)
 
-    @_step
     @screen_manager.with_screen
-    def compare_part(self, coords, screen: ndarray) -> bool:
-        cropped_screen = self._crop_screen(screen, coords)
-        return self._compare_loop(cropped_screen, compare_methods.ssim)
-
     @_step
-    def tap(self):
-        actions.input_tap([500, 500])
-        
+    def compare_part(self, screen, coords) -> bool:
+        cropped_screen = self._crop_screen(screen, coords=coords)
+        return self._compare_loop(cropped_screen, compare_methods.ssim)
