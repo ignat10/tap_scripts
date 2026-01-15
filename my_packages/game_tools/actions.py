3b5abb4eabb420a7973938adf78900285151fe4a
Ignat10 <ignat.shikh@gmail.com>
2026-01-15 19:57:19 +0100

    timing calibration

--- my_packages/game_tools/actions.py
+++ my_packages/game_tools/actions.py
@@ -12,16 +12,16 @@ from ..utils.inputter import inputter
 
 
 class Farm:
-    alliances_elite_mines: dict[Self, int] = {}
+    alliances_elite_mines: dict[str, int] = {}
 
     def __init__(self, name: str, lv: int, google: int, account: int, alliance: str):
-        self.name = name
+        self.name = name.replace(".", "")
         self.google = google
         self.account = account
         self.lv = lv
         self.alliance = alliance
         self.mine_lv = 6
-        self.mine_type = 0
+        self.mine_type = 3
 
     @staticmethod
     def lord_skills():
@@ -32,21 +32,22 @@ class Farm:
         print("harvested. recalling...")
         objects["recall_all"].click()
         objects["use"].click(delay=0.5)
-        objects["close"].click()
-        objects["close"].click()
+        objects["cities"].click(delay=1)
+        objects["cities"].click()
         print("lord skills done.")
 
-    def get_std_mine(self):  # to go to basic mine from the map
+    def get_std_mine(self) -> None:  # to go to basic mine from the map
 
         def find_another_mine() -> None:  # to find another mine if not found
-            objects["mine_type"].click(steps=self.mine_type)
+            print(f"searching mine type: {self.mine_type}, lv: {self.mine_lv}")
+            objects["mine_type"].click(steps=self.mine_type, delay=0.5)
             objects["minus"].click(repeat=5)
             objects["plus"].click(repeat=self.mine_lv - 1)
-            objects["go_mine"].click(repeat=3)
+            objects["go"].click(repeat=3)
 
         def gather_std_mine() -> None:
             objects["gather"].click(delay=0.5)
-            objects["gather"].click(delay=0.5)
+            objects["go"].click(delay=0.5)
             objects["back"].click(delay=0.5)
 
         objects["search"].click(delay=1)
@@ -62,31 +63,30 @@ class Farm:
                     objects["gather"].click()
                     break
                 case status.Status.NOT_FOUND:
-                    if self.mine_type < 4:
+                    if self.mine_type > 0:
                         print("second mine type")
-                        self.mine_type += 1
+                        self.mine_type -= 1
                     else:
                         print("less lv")
                         self.mine_lv -= 1
-                        self.mine_type = 0
+                        self.mine_type = 3
                 case status.Status.NOT_MAP:
                     print("somehow I'm not at the map.\npanic")
                     continue
         objects["mine"].click(delay=0.5)
-        sleep(2)
         gather_std_mine()
 
-    def get_elite_mine(self):
+    def get_elite_mine(self) -> bool:
         print("Elite")
         while True:
             objects["book"].click()
             objects["alliance_elite_mines"].click(delay=0.5)
             sleep(1)
-            if objects["blue"].compare_part():  # color of blue
-                objects["blue"].click(steps=self.alliances_elite_mines[self])
-                objects["gather_elite_mine"].click(delay=3)
+            if objects["blue"].compare_part(steps=self.alliances_elite_mines.setdefault(self.alliance, 0)):  # color of blue
+                objects["blue"].click(steps=self.alliances_elite_mines[self.alliance])
+                objects["gather_elite_mine"].click(delay=2)
                 objects["go"].click(delay=1)  # regularly I should be there
-                self.alliances_elite_mines[self] += 1
+                self.alliances_elite_mines[self.alliance] += 1
                 return True  # everything is alright I went to elite
             else:
                 print("some chemistry error")
@@ -110,14 +110,15 @@ class Farm:
             print(f"already {self.name}")
         else:
             self.switch_account()            
-            while objects["load"].compare_part():
+            while not objects[self.name].compare_part():
+                objects["xs"].find_and_click(objects["map"])
                 print(f"loading {self.name}...")
             print(f"loaded {self.name}")
 
     @staticmethod
     def go_outside():
         print("Going to the map...")
-        objects["map"].click(repeat=5)
+        objects["map"].click(repeat=3)
         sleep(2)
         while not objects["book"].compare_part():
             objects["xs"].find_and_click(base_object=objects['map'])
@@ -127,7 +128,7 @@ class Farm:
 
 def castle_generator():
     sheet = openpyxl.load_workbook(FARMS_SHEET_PATH).active
-    for row in sheet.iter_rows(min_row=inputter("enter from which google do we start: ", 1) + 1, values_only=True):
+    for row in sheet.iter_rows(min_row=inputter("enter from which castle do we start: ", 1) + 1, values_only=True):
         yield Farm(*row)
 
 
