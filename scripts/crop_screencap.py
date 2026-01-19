e671d61caa12b3ac0200a53f214a14c0c17a7c75
Ignat10 <Ignat.shikh@gmail.com>
2026-01-20 00:39:24 +0100

    edit coords

--- scripts/crop_screencap.py
+++ scripts/crop_screencap.py
@@ -1,21 +1,21 @@
 from os import system
 
-
 from cv2 import imread, imwrite
-from numpy import ndarray
 
+from src.device import actions
+from src.game_tools.objects import objects
 
-from my_packages.device import actions
-from my_packages.game_tools.objects import objects
-coords = (505, 1016)
 
 
 actions.config_serial()
 system(f"adb -s {actions.serial} exec-out screencap -p > screen.png")
 
-avatar = objects["cities"]
+
+template = objects["cities"]
+
+
 screen = imread("screen.png")
 
-crop = avatar._crop_screen(screen, coords)
+crop = template._crop_screen(screen)
 
-imwrite("cropped_ava.png", crop)
+imwrite("cropped_screen.png", crop)
