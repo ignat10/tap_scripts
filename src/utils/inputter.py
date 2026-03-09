f7d0da315325950db48b456981e5e0a57bdc7de6
Ignat10 <Ignat.shikh@gmail.com>
2026-03-09 15:02:54 +0100

    add typeVar

--- src/utils/inputter.py
+++ src/utils/inputter.py
@@ -1,7 +1,13 @@
-def inputter(msg: str, base):
-    type_ = type(base)
+from typing import TypeVar
+
+
+T = TypeVar("T")
+
+
+def inputter(msg: str, base_val: T) -> T:
+    type_ = type(base_val)
     string = input(msg)
     try:
         return type_(string)
     except ValueError:
-        return base
+        return base_val
