03d1fa3ead1881c9fb9028f85494d7c00063347f
Ignat10 <Ignat.shikh@gmail.com>
2026-03-15 18:14:56 +0100

    working type hints

--- scripts/compare_blue.py
+++ scripts/compare_blue.py
@@ -1,11 +1,9 @@
 import timeit
 
-import image_comparer
-print(image_comparer.sum_as_string(4, 7))
-from src.game_object.objects import objects
 
 
-book = objects["blue"]
-
-
-print(timeit.timeit('book.quick_compare()', setup = 'from src.game_object.objects import objects; book = objects["blue"]; book.quick_compare()', number=10000))
+print(timeit.timeit(
+    'book.quick_compare()',
+    setup = 'from src.game_object.objects import objects; book = objects["blue"]; book.quick_compare()',
+    number=10000
+))
