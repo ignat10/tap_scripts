eab71e6c2f3af89ad38d6b84c5cab8fc5c6cfd26
ignat10 <ignat.shikh@gmail.com>
2026-06-07 18:12:23 +0200

    Remove OBJECTS_PATH definition from paths.py

--- src/paths.py
+++ src/paths.py
@@ -8,4 +8,3 @@ _ROOT_DIR = Path(__file__).parent.parent
 DATA_DIR = _ROOT_DIR / 'data'
 SAMPLES_DIR = DATA_DIR / 'samples'
 FARMS_SHEET_PATH = DATA_DIR / 'WAO_farms_data.xlsx'
-OBJECTS_PATH = DATA_DIR / 'game_objects.json'
