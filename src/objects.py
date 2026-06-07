41dc4e3cc73b2a462187ca84298e1bf075be358f
Ignat10 <ignat.shikh@gmail.com>
2026-06-07 17:51:27 +0200

    fix .env

--- src/objects.py
+++ src/objects.py
@@ -82,7 +82,8 @@ objects: dict[ScreenObjectNames, ScreenObject] = cast(
 def config():
     load_dotenv()
     adb = getenv("ADB")
+    ip = getenv("IP")
     if adb is None:
-        raise Exception("ADB not set")
-    device_config(adb=Path(adb), ip=None)
+        raise Exception("ADB not set. set adb path in .env file.")
+    device_config(adb=Path(adb), ip=ip)
     
